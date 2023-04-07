584c9e5dfcde9db42696e59bc7458042a27c4ff1
Tan Peng Teck <pttan2001@gmail.com>
2023-04-07 17:38:49 +0800

    hi

--- EDA.R
+++ EDA.R
@@ -13,17 +13,26 @@ sum(is.na(airline))
 # Plot customer satisfaction by gender
 library(dplyr)
 library(ggplot2)
+library(forcats)
 
-airline %>%
-  group_by(Gender, satisfaction) %>%
-  summarise(n = n()) %>%
-  mutate(perc = n / sum(n) * 100) %>%
-  ggplot(aes(x = satisfaction, y = n, fill = Gender)) +
-  geom_bar(stat = "identity", position = "stack", width = 0.35) +
+
+airline |>
+  group_by(Gender, satisfaction) |>
+  summarise(n = n()) |>
+  mutate(perc = n / sum(n) * 100) |>
+  ggplot(aes(x = satisfaction, y = n, fill = satisfaction)) +  # Updated fill aesthetics to satisfaction
+  geom_bar(stat = "identity",
+           position = "stack",
+           width = 0.35) +
   geom_text(aes(label = paste0(round(perc), "%")), position = position_stack(vjust = 0.5)) +
   labs(title = "Customer Satisfaction by Gender", x = "Satisfaction", y = "Count") +
+  facet_grid(. ~ Gender,
+             scales = "free_x",
+             space = "free_x",
+             switch = "x") +  # Added facet grid for Gender
   scale_x_discrete(labels = c("Dissatisfied", "Satisfied")) +
-  scale_fill_discrete(name = "Gender", labels = c("Female", "Male")) +
+  scale_fill_discrete(name = "Satisfaction",
+                      labels = c("Dissatisfied", "Satisfied")) +  # Updated fill labels
   theme_minimal() +
   theme(
     plot.title = element_text(size = 16, hjust = 0.5),
@@ -31,7 +40,7 @@ airline %>%
     axis.text = element_text(size = 10),
     legend.title = element_text(size = 12),
     legend.text = element_text(size = 10)
-  ) + coord_flip()
+  )
 
 # The plot shows that more customers are satisfied than dissatisfied.
 # Among males, 56% are dissatisfied and 44% are satisfied.
@@ -43,4 +52,3 @@ airline %>%
 # and its customers.
 # However, it could be due to differences in expectations or
 # experiences between male and female customers.
-
